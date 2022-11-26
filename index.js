791d86d8ac81bf217ae4691e2dd6578864ae9cc5
mostafiz <rafi11zxy@gmail.com>
2022-11-26 18:31:52 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", async(req,res) => {
+    res.send("server is running");
+});
+
+
+
+
+
+
+
+app.listen(port,() => {
+    console.log("server is running on " + port);
+})
