f07cce5002a08fb632e3fecf0feb182631fc3999
mostafiz <rafi11zxy@gmail.com>
2022-12-01 03:10:11 +0600

    jwt added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
@@ -9,6 +10,23 @@ const app = express();
 // middleware
 app.use(cors());
 app.use(express.json());
+
+function verifyJWT(req, res, next) {
+  // console.log("token inside verifyjwt ", req.headers.authorization);
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send("unauthorized access");
+  }
+  const token = authHeader.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN, function(err, decoded){
+    if(err){
+      return res.status(403).send({message: "forbiddeen access"})
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
 
 // mongodb
 
@@ -27,19 +45,37 @@ async function run() {
     const userCollection = client.db("userCollection").collection("users");
 
     // get user specific order list
-    app.get("/myorder", async (req,res) => {
+    app.get("/myorder", verifyJWT, async (req, res) => {
       // console.log(req.query);
       const email = req.query.email;
+      const decodedEmail = req.decoded.email;
+      if(email !== decodedEmail){
+        return res.status(403).send({message: "forbiddeen access"});
+      }
       // console.log(email);
-      const query = {email : email};
+      const query = { email: email };
       const myOrderGet = await myOrder.find(query).toArray();
       res.send(myOrderGet);
     });
-  
+
     app.get("/", async (req, res) => {
       const query = {};
       const options = await catagories.find(query).toArray();
       res.send(options);
+    });
+
+    // jwt tokent
+    app.get("/jwt", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      if (user) {
+        const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, {
+          expiresIn: "1h",
+        });
+        return res.send({ accessToken: token });
+      }
+      res.status(403).send({ accessToken: "" });
     });
 
     // get catagory based data
@@ -52,8 +88,7 @@ async function run() {
       // console.log(id);
     });
 
-    // 
-    
+    //
 
     // post my order details
     app.post("/myorder", async (req, res) => {
@@ -64,16 +99,12 @@ async function run() {
     });
 
     // post user info details
-    app.post("/users", async (req,res) => {
+    app.post("/users", async (req, res) => {
       const user = req.body;
       const result = await userCollection.insertOne(user);
       res.send(result);
     });
-
-  
-    
-  } catch {
-  }
+  } catch {}
 }
 run().catch(console.log);
 
